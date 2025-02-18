901c16729d422bd51e3f46c112399c3707b4a55e
Egi Pratama <egila391@gmail.com>
2025-02-18 11:00:27 +0700

    feat: react map & navigator

--- react-js/src/App.jsx
+++ react-js/src/App.jsx
@@ -1,15 +1,13 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Map from "./Pages/Maps";
 import Navigator from "./pages/Navigator";
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Map />} />
-        <Route path="/navigator" element={<Navigator />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Map />} />
+      <Route path="/navigator" element={<Navigator />} />
+    </Routes>
   );
 }
 
