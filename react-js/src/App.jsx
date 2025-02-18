33349ac46b067ff81d8004caf2b2782602f05865
Egi Pratama <egila391@gmail.com>
2025-02-18 11:17:03 +0700

    feat(react): Display Polygon

--- react-js/src/App.jsx
+++ react-js/src/App.jsx
@@ -4,6 +4,7 @@ import Navigator from "./pages/Navigator";
 import Marker from "./Pages/Marker";
 import Points from "./Pages/Points";
 import Line from "./Pages/Line";
+import Polygon from "./Pages/Polygon";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/marker" element={<Marker />} />
       <Route path="/points" element={<Points />} />
       <Route path="/line" element={<Line />} />
+      <Route path="/polygon" element={<Polygon />} />
     </Routes>
   );
 }
