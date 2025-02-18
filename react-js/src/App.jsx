1292ef2233dd3395610b93e59bfb1dc3c1f8f6bc
Egi Pratama <egila391@gmail.com>
2025-02-18 11:05:25 +0700

    feat: react marker

--- react-js/src/App.jsx
+++ react-js/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Map from "./Pages/Maps";
 import Navigator from "./pages/Navigator";
+import Marker from "./Pages/Marker";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Map />} />
       <Route path="/navigator" element={<Navigator />} />
+      <Route path="/marker" element={<Marker />} />
     </Routes>
   );
 }
