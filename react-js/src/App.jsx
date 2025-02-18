29e99b2301d1844a22411d0390ce62edcf897c01
Egi Pratama <egila391@gmail.com>
2025-02-18 11:10:48 +0700

    feat(react): points from geojson

--- react-js/src/App.jsx
+++ react-js/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Map from "./Pages/Maps";
 import Navigator from "./pages/Navigator";
 import Marker from "./Pages/Marker";
+import Points from "./Pages/Points";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Map />} />
       <Route path="/navigator" element={<Navigator />} />
       <Route path="/marker" element={<Marker />} />
+      <Route path="/points" element={<Points />} />
     </Routes>
   );
 }
